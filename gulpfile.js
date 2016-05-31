2840fc23261a2932fcdb0d0581cf02c81f0d5558
Milan Matull <milan@milanmatull.com>
2016-05-31 17:48:43 +0200

    Configure postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,27 @@
-var gulp = require('gulp');
+const gulp = require('gulp')
 
-gulp.task('default', function() {
-  // place code for your default task here
-});
+const postcss = require('gulp-postcss')
+const postCssImport = require('postcss-import')
+const postCssUrl = require('postcss-url')
+const postCssCustomProperties = require('postcss-custom-properties')
+const postCssCalc = require('postcss-calc')
+const postCssColorFunction = require('postcss-color-function')
+const postCssCustomMedia = require('postcss-custom-media')
+const postCssPseudoElements = require('postcss-pseudoelements')
+const autoprefixer = require('autoprefixer')
+
+gulp.task('css', function() {
+  const processors = [
+    postCssImport,
+    postCssUrl,
+    postCssCustomProperties,
+    postCssCalc,
+    postCssColorFunction,
+    postCssCustomMedia,
+    postCssPseudoElements,
+    autoprefixer
+  ]
+  return gulp.src('./src/index.css')
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./dist'))
+})
