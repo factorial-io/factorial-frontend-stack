daf6e9f517fcc0e380d4ee16cad2e23dacaea1f9
Simon Reinhardt <s_reinhardt@icloud.com>
2017-02-02 16:35:51 +0100

    Import Stylefmt

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -14,6 +14,7 @@ import sourcemaps from 'gulp-sourcemaps';
 import stylelint from 'gulp-stylelint';
 import watchify from 'watchify';
 import ghPages from 'gulp-gh-pages';
+import stylefmt from 'gulp-stylefmt';
 
 const processors = [
   require('postcss-import'),
