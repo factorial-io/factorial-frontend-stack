4afcaa22d849b4ff3330b2f3f04731c756a21b88
Milan Matull <milan@factorial.io>
2018-10-30 16:57:51 +0100

    Simple eslint-disable format

--- examples/core/src/index.js
+++ examples/core/src/index.js
@@ -1,12 +1,14 @@
+/* eslint-disable no-console */
+
 import { format } from "date-fns";
 import { square, merge } from "./lib/math";
 import "./index.css"; // starts CSS dependency chain
 
 const s = square(2);
 
-console.log(s); // eslint-disable-line
-console.log(merge([1, 2, 3], [4, 5, 6])); // eslint-disable-line
-console.log(format(new Date(), 'd.m.Y')); // eslint-disable-line
+console.log(s);
+console.log(merge([1, 2, 3], [4, 5, 6]));
+console.log(format(new Date(), "d.m.Y"));
 
-// Test if window object is considerer global
-console.log(window); // eslint-disable-line no-console
+// Test if window object is set to global
+console.log(window);
