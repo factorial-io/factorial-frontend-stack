88d9540f92de6125aefbbfdc280340d8ebd21741
Milan Matull <milan@factorial.io>
2019-01-22 16:16:30 +0100

    Add first version of postcss export

---
+++ packages/postcss-export-custom-variables/README.md
@@ -0,0 +1,39 @@
+# Factorial frontend stack core
+
+> `@factorial/frontend-stack-postcss-export-custom-variables` is Neutrino middleware for [postcss-export-custom-variables](https://github.com/jonathantneal/postcss-export-custom-variables)
+
+## Features
+
+- Exports PostCSS variables to a JSON file for further consumption in a twig/js tool chain.
+
+## Requirements
+
+- Node.js ^8.10 or 10+
+- Yarn v1.2.1+, or npm v5.4+
+- Neutrino v8
+
+## Installation
+
+`@factorial/frontend-stack-postcss-export-custom-variables` can be installed via the Yarn or npm clients.
+
+## Usage
+
+    yarn add neutrino @factorial/frontend-stack-core @factorial/frontend-stack-postcss-export-custom-variables --dev
+
+Then add the following your `package.json`.
+
+```json
+"scripts": {
+  "start": "yarn run neutrino start --use @factorial/frontend-stack-core @factorial/frontend-stack-postcss-export-custom-variables",
+  "build": "yarn run neutrino build --use @factorial/frontend-stack-core @factorial/frontend-stack-postcss-export-custom-variables --options.env.NODE_ENV production"
+}
+```
+
+## Options
+
+This middleware exposed the same options as [postcss-export-custom-variables](https://github.com/jonathantneal/postcss-export-custom-variables#advanced-options), but sets the following defaults:
+
+name | value
+---|---
+`destination` | "./source/_patterns/01-theme/theme.json"
+`exporter` | A custom JSON export that wraps all properties in a "variables" key.
---
+++ packages/postcss-export-custom-variables/index.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const fs = require("fs");
+
+// Just wraps everything in  a "variables" key so we can iterate over it in twig
+// @see: https://github.com/jonathantneal/postcss-export-custom-variables/blob/master/index.js#L125
+function jsonExporter(variables, options, root) {
+	const pathname = options.destination || root.source && root.source.input && root.source.input.file && root.source.input.file + '.json' || 'custom-variables.json';
+	const contents = JSON.stringify({ "variables": variables }, null, '  ');
+
+	return new Promise((resolve, reject) => {
+		fs.writeFile(
+			pathname,
+			contents,
+			(error) => error ? reject(error) : resolve()
+		);
+	});
+}
+
+module.exports = (neutrino, options = {}) => {
+  // Set default options, based on pattern lab use case
+  // @see: https://github.com/jonathantneal/postcss-export-custom-variables#postcss
+  const defaults = {
+    destination: path.resolve("source/_patterns/01-theme/theme.json"),
+    exporter: jsonExporter,
+  }
+
+  // Merge options with defaults
+  const pluginOptions = {
+    ...defaults,
+    ...options,
+  };
+
+  // Get the existing style rules
+  const styleRule = neutrino.config.module.rule('style');
+
+  // Save a reference to the existing style loaders
+  const styleLoaders = Array.from(styleRule.uses.store.keys())
+    .filter((key) => key !== 'style')
+    .map((key) => styleRule.use(key))
+    .map((use) => {
+      return {
+        loader: use.get('loader'),
+        options: use.get('options')
+      };
+    });
+
+  // Clear the existing style loaders
+  styleRule.uses.clear();
+
+  // Iterate over the saved references
+  styleLoaders.forEach((styleLoader) => {
+    const { loader, options } = styleLoader;
+
+    // When we match with postcss loader, add the exporter to the set
+    // of PostCSS plugins
+    if (path.dirname(loader).indexOf("postcss-loader") > -1) {
+      options.plugins.push(require("postcss-export-custom-variables")(pluginOptions));
+    }
+
+    // Add style rules again
+    styleRule.use(loader).loader(loader).options(options);
+  });
+};
