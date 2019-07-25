d0ef217deb8fd6c56a4398d245ab93879152f18b
Andrew Beng <andrewbeng89@gmail.com>
2019-07-25 09:14:26 +0200

    More tests

--- examples/core/src/lib/math.spec.js
+++ examples/core/src/lib/math.spec.js
@@ -5,4 +5,13 @@ describe("Test math lib functions", () => {
     expect(math.square(2)).toBe(4);
     expect(math.square(3)).toBe(9);
   });
+
+  it("should return the cube of a number", () => {
+    expect(math.cube(2)).toBe(8);
+    expect(math.cube(3)).toBe(27);
+  });
+
+  it("should merge two arrays", () => {
+    expect(math.merge([1, 2, 3], [4, 5, 6])).toEqual([1, 2, 3, 4, 5, 6]);
+  });
 });
