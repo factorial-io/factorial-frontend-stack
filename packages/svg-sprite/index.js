a819f18b20851eae3d265b96238e93d456875e7c
Kris Immig <kris@factorial.io>
2019-10-22 17:32:45 +0200

    Add svg-sprite loader package (WIP)

---
+++ packages/svg-sprite/index.js
@@ -0,0 +1,23 @@
+module.exports = (neutrino, opts = {}) => {
+  neutrino.config.module.rules.delete('svg');
+
+  neutrino.config.module
+    .rule('svg')
+      .clear()
+      .pre()
+      .test(neutrino.regexFromExtensions(['svg']))
+      .include
+        .add(neutrino.options.source)
+        .end()
+      .use('svg-sprite')
+        .loader(require.resolve('svg-sprite-loader'))
+        .end()
+      .use('svgo')
+        .loader(require.resolve('svgo-loader'))
+          .options({
+            plugins: [
+              { removeTitle: true },
+              { removeUselessStrokeAndFill: true }
+            ]
+          })
+};
